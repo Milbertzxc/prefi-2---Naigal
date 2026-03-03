acbdaac4b88079cd3fe1c62e79c98b91b366692d
Milbertzxc <laxus123455@gmail.com>
2026-03-03 17:08:30 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -1,16 +1,46 @@
-function generateFullName() {
-    var first = document.getElementById("firstName").value;
-    var middle = document.getElementById("middleName").value;
-    var last = document.getElementById("lastName").value;
+var numbersArray = [];
 
-    var fullName = first + " " + middle + " " + last;
+function insertNumber() {
+    var value = parseFloat(document.getElementById("numberInput").value);
 
-    document.getElementById("fullName").innerHTML = fullName;
+    if (!isNaN(value)) {
+        numbersArray.push(value);
+
+        document.getElementById("numbers").innerHTML = numbersArray.join(", ");
+
+        calculate();
+    }
+
+    document.getElementById("numberInput").value = "";
 }
 
-function clearEntries() {
-    document.getElementById("firstName").value = "";
-    document.getElementById("middleName").value = "";
-    document.getElementById("lastName").value = "";
-    document.getElementById("fullName").innerHTML = "";
+function calculate() {
+    var sum = 0;
+    var highest = numbersArray[0];
+    var lowest = numbersArray[0];
+
+    for (var i = 0; i < numbersArray.length; i++) {
+        sum += numbersArray[i];
+
+        if (numbersArray[i] > highest) {
+            highest = numbersArray[i];
+        }
+
+        if (numbersArray[i] < lowest) {
+            lowest = numbersArray[i];
+        }
+    }
+
+    document.getElementById("sum").innerHTML = sum;
+    document.getElementById("highest").innerHTML = highest;
+    document.getElementById("lowest").innerHTML = lowest;
+}
+
+function deleteAll() {
+    numbersArray = [];
+
+    document.getElementById("numbers").innerHTML = "";
+    document.getElementById("sum").innerHTML = "";
+    document.getElementById("highest").innerHTML = "";
+    document.getElementById("lowest").innerHTML = "";
 }
