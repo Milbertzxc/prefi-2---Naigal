3945209e4a77340ef5300a3cd5bf6af51c3c6617
Milbertzxc <laxus123455@gmail.com>
2026-03-03 17:04:35 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -1,54 +1,16 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>JS Exercise 1</title>
-</head>
-<body>
+function generateFullName() {
+    var first = document.getElementById("firstName").value;
+    var middle = document.getElementById("middleName").value;
+    var last = document.getElementById("lastName").value;
 
-    <h1>JS Exercise 1</h1>
+    var fullName = first + " " + middle + " " + last;
 
-    <label>Enter First Number:</label>
-    <input type="text" id="firstNumber"><br><br>
+    document.getElementById("fullName").innerHTML = fullName;
+}
 
-    <label>Enter Second Number:</label>
-    <input type="text" id="secondNumber"><br><br>
-
-    <button onclick="calculate()">Calculate</button>
-    <button onclick="clearAll()">Clear</button>
-
-    <br><br>
-
-    <p>Sum: <span id="sum"></span></p>
-    <p>Difference: <span id="difference"></span></p>
-    <p>Product: <span id="product"></span></p>
-    <p>Quotient: <span id="quotient"></span></p>
-
-    <script>
-        function calculate() {
-
-            var num1 = parseFloat(document.getElementById("firstNumber").value);
-            var num2 = parseFloat(document.getElementById("secondNumber").value);
-
-            var sum = num1 + num2;
-            var difference = num1 - num2;
-            var product = num1 * num2;
-            var quotient = num1 / num2;
-
-            document.getElementById("sum").innerHTML = sum;
-            document.getElementById("difference").innerHTML = difference;
-            document.getElementById("product").innerHTML = product;
-            document.getElementById("quotient").innerHTML = quotient;
-        }
-
-        function clearAll() {
-            document.getElementById("firstNumber").value = "";
-            document.getElementById("secondNumber").value = "";
-            document.getElementById("sum").innerHTML = "";
-            document.getElementById("difference").innerHTML = "";
-            document.getElementById("product").innerHTML = "";
-            document.getElementById("quotient").innerHTML = "";
-        }
-    </script>
-
-</body>
-</html>
+function clearEntries() {
+    document.getElementById("firstName").value = "";
+    document.getElementById("middleName").value = "";
+    document.getElementById("lastName").value = "";
+    document.getElementById("fullName").innerHTML = "";
+}
